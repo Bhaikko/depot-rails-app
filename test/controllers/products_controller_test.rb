9a49a1b0aed4a6b8cd7391272822ae53478b5a78
siddharth <siddhartha.pawar17@gmail.com>
2022-07-19 21:53:20 +0530

    Unit Tests for Product Model and Controller added - Testing data as fixtures added in test/fixtures/products.yml - Unit tests for Validation of Product data added in test/models/product_test.rb - Unit tests for Product controller added in test/controllers/products_controller_test.rb

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,11 @@ require "test_helper"
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+
+    # Creating a random title for insertion
+    # Since name in database will be unique, and @product title already exists
+    # Hence need to create random title
+    @title = "The Great Book #{rand(1000)}"
   end
 
   test "should get index" do
@@ -17,7 +22,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference("Product.count") do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      post products_url, params: { 
+        product: { 
+          description: @product.description, 
+          image_url: @product.image_url, 
+          price: @product.price, 
+          title: @title
+        } 
+      }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -34,7 +46,15 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch product_url(@product), params: { 
+      product: { 
+        description: @product.description, 
+        image_url: @product.image_url, 
+        price: @product.price, 
+        title: @title
+      }
+    }
+    
     assert_redirected_to product_url(@product)
   end
 
