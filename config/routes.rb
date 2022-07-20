fcc9553ab0b44d1ffbf577d2059926eb53bfb40a
siddharth <siddhartha.pawar17@gmail.com>
2022-07-20 18:46:06 +0530

    Scaffolding and Schema migration for LineItems added - Model, Controller and View for LineItems created - Route configuration added for LineItems added - Tests for LineItems created - migration for LineItems applied to database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :line_items
   resources :carts
   # Creating Store as Root URL of App 
   # as: option creates store_index_path and store_index_url methods for tests
