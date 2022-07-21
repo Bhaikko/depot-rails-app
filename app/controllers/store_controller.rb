14dc33ef4c0f025a81667a1749c1ee42079761a2
siddharth <siddhartha.pawar17@gmail.com>
2022-07-21 15:48:50 +0530

    Cart added as Partial in application sidebar - Updated store controller to have Cart in its context - Added line_item and cart as partial templates - Partial template of cart added in sidebar of application

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,8 @@
 class StoreController < ApplicationController
+  include CurrentCart
+
+  before_action :set_cart
+
   def index
     # Getting List of Products ordered by title
     @products = Product.order(:title)
