ff11524084a8f94655d354633f2f9ca224df307a
siddharth <siddhartha.pawar17@gmail.com>
2022-07-21 14:05:04 +0530

    Test updated for line_items controller

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -25,8 +25,8 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     ## performing redirect on location
     follow_redirect!
 
-    assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', "1 \u00D7 Programming Ruby 1.9"
+    assert_select 'h2', 'Your Cart'
+    assert_select 'td', "Programming Ruby 1.9"
   end
 
   test "should show line_item" do
