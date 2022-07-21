85605c460bd2604e1fd31032a4d15151a91c177f
siddharth <siddhartha.pawar17@gmail.com>
2022-07-21 11:31:19 +0530

    Minor bug fix when adding product to cart as line_item

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,16 +3,4 @@ class LineItem < ApplicationRecord
   # Using these, product and cart info can be retrieved using line_item
   belongs_to :product
   belongs_to :cart
-
-  def add_product(product)
-    current_item = line_items.find_by(product_id: product.id)
-    if current_item
-      current_item.quantity += 1
-    else
-      # build method builds a lineitem with relationship between line_item object and product
-      current_item = line_items.build(product_id: product.id)
-    end
-
-    current_item
-  end
 end
