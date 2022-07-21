dde820b4e6747843f88581010220177700058358
siddharth <siddhartha.pawar17@gmail.com>
2022-07-21 11:31:19 +0530

    Minor bug fix when adding product to cart as line_item

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,16 @@ class Cart < ApplicationRecord
   ## Allows traversing from parent to child in relation
   # dependent declares ON DELETE CASCADE dependency over lineitems for a cart
   has_many :line_items, dependent: :destroy
+
+  def add_product(product)
+    current_item = line_items.find_by(product_id: product.id)
+    if current_item
+      current_item.quantity += 1
+    else
+      # build method builds a lineitem with relationship between line_item object and product
+      current_item = line_items.build(product_id: product.id)
+    end
+
+    current_item
+  end
 end
