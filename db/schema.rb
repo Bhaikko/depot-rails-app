42b3922bd083ebcaca49fb030a0529efa261c657
siddharth <siddhartha.pawar17@gmail.com>
2022-07-21 11:33:44 +0530

    Schema migration added to update data in line_items - The migration will update the quantity of all items in cart based on number of times a list item is present for a cart - both up and down methods created to apply and rollback migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_21_050938) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_053603) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
