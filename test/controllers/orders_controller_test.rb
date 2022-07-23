1f93def69f8af93c861fd38165ea3adec77ede6d
siddharth <siddhartha.pawar17@gmail.com>
2022-07-23 12:36:08 +0530

    Functional tests update for Orders

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
   setup do
+    ## Common place to set up object for all tests
     @order = orders(:one)
   end
 
@@ -11,7 +12,7 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-    post line_items_url, params: { product_id: products[:ruby].id }
+    post line_items_url, params: { product_id: products(:ruby).id }
     
     get new_order_url
     assert_response :success
@@ -19,10 +20,17 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create order" do
     assert_difference("Order.count") do
-      post orders_url, params: { order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type } }
+      post orders_url, params: { 
+        order: { 
+          address: @order.address,
+          email: @order.email, 
+          name: @order.name, 
+          pay_type: @order.pay_type 
+        } 
+      }
     end
 
-    assert_redirected_to order_url(Order.last)
+    assert_redirected_to store_index_url
   end
 
   test "should show order" do
