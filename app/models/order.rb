7780ad880777d8d4a3d9852e1878e30bf8f70db7
siddharth <siddhartha.pawar17@gmail.com>
2022-07-23 11:32:25 +0530

    validation for New Order form added in order Model

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,8 @@ class Order < ApplicationRecord
     "Credit card"     => 1,
     "Purchase order"  => 2
   }
+
+  validates :name, :address, :email, presence: true
+  # validating key types as user can still submit form directly from outside 
+  validates :pay_type, inclusion: pay_type.keys
 end
