2a0834ead634ca389cd5951005eeaec391a27032
siddharth <siddhartha.pawar17@gmail.com>
2022-07-25 14:17:10 +0530

    System test for payment job added

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,6 +1,9 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
+  # included to test jobs
+  include ActiveJob::TestHelper
+
   setup do
     @order = orders(:one)
   end
@@ -47,11 +50,18 @@ class OrdersTest < ApplicationSystemTestCase
 
   # This test will not work for now because of webpack issue of not rendering component first time
   test "check routing number" do
+    # Deleting any lineitem or Order created by other tests
+    LineItem.delete_all
+    Order.delete_all
+
     visit store_index_url
 
     click_on 'Add to Cart', match: :first
     
     click_on 'Checkout'
+
+    # To get around refreshing bug of form
+    visit order_index_url
 
     fill_in 'order_name', with: 'Dave Thomas'
     fill_in 'order_address', with: '123 Main'
@@ -64,5 +74,36 @@ class OrdersTest < ApplicationSystemTestCase
 
     ## Checking if element with if #order_routing_number is on screen
     assert_selector "#order_routing_number"
+
+    fill_in "Routing #", with: "123456"
+    fill_in "Account #", with: "987654"
+
+    # Making sure the jobs get executed after perfoming actions inside block
+    perform_enqueued_jobs do
+      # Rails will execute all the jobs queue because of this block code
+      click_button "Place Order"
+    end
+
+    ## Checking if Order was created
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal "Dave Thomas", order.name
+    assert_equal "123 Main Street", order.address
+    assert_equal "dave@example.com", order.email
+    assert_equal "Check", order.pay_type
+
+    assert_equal 1, order.line_items.size
+
+    ## Testing if mails was sent
+    # In testing env, mails are not sent but stored in ActionMailer::Base.deliveries()
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["dave@example.com"], mail.to
+    assert_equal 'Sam Ruby <depot@example.com>', mail[:from].value
+    assert_equal 'Pragmatic Store Order Confirmation', mail.subject
   end
+
+
 end
