7feb85a84da1c726016303afbe138c492fb7e8a2
siddharth <siddhartha.pawar17@gmail.com>
2022-07-26 15:38:40 +0530

    Conductor used for sending Mails to Rails server fixed

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,20 @@ module Depot
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Fix for Conductor throwing undefined method error
+    config.to_prepare do 
+      Rails::Conductor::ActionMailbox::InboundEmailsController.class_eval do
+        private
+        def new_mail
+          Mail.new(mail_params.except(:attachments).to_h).tap do |mail|
+            mail[:bcc]&.include_in_headers = true
+            mail_params[:attachments].to_a.compact_blank.each do |attachment|
+              mail.add_file(filename: attachment.original_filename, content: attachment.read)
+            end
+          end
+        end
+      end    
+    end
   end
 end
