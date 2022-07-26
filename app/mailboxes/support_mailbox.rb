9d7a0fd07d36e52c0c364968720e5f6dbbf09f71
siddharth <siddhartha.pawar17@gmail.com>
2022-07-26 16:00:20 +0530

    Mails from Mailbox storable in Support Requests table

--- app/mailboxes/support_mailbox.rb
+++ app/mailboxes/support_mailbox.rb
@@ -1,10 +1,17 @@
 class SupportMailbox < ApplicationMailbox
   # Every email received at this route will trigger process method
   def process
-    puts "START SupportMailbox#process:"
-    puts "From: #{mail.from_address}"
-    puts "Subject: #{mail.subject}"
-    puts "Body: #{mail.body}"
-    puts "END SupportMailbox#process:"
+    recent_order = Order
+      .where(email: mail.from_address.to_s)
+      .order('created_at desc')
+      .first
+    
+    SupportRequest.create!(
+      email: mail.from_address.to_s,
+      subject: mail.subject,
+      body: mail.body.to_s,
+      order: recent_order
+    )
+
   end
 end
--- app/models/support_request.rb
+++ app/models/support_request.rb
@@ -1,3 +1,4 @@
 # Will store received emails on Support route
 class SupportRequest < ApplicationRecord
+  belongs_to :order, optional: true
 end
