a15bbc21f815998df5b0137266a9f8fef7a18302
Siddharth Pawar <42922086+Bhaikko@users.noreply.github.com>
2022-07-29 22:05:17 +0530

    5. Make form fields in product creation form for: 	a. enabled(checkbox) 	b. discount_price(Input Field) 	c. permalink(Input Field)
    	- Controller updated to permit above attributes of product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -90,7 +90,10 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:title, :description, :image_url, :price)
+      params.require(:product).permit(
+        :title, :description, :image_url, :price,
+        :enabled, :discount_price, :permalink
+      )
     end
 
     def invalid_product
