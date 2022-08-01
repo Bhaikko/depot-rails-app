9ec5ab8239afd695d7348d26ba938f15f38bd8cc
Siddharth Pawar <42922086+Bhaikko@users.noreply.github.com>
2022-08-01 17:41:45 +0530

    6. User Model Validations added for Email 	a. Uniqueness of Email validation added 	b. Email format using Regex added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i
+
   validates :name, presence: true, uniqueness: true
+
+  validates :email, uniqueness: true, format: {
+    with: VALID_EMAIL_REGEX
+  }
 
   # Validates that the two passwords match in field
   has_secure_password
