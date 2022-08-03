8d69d94fbcb79cb51e49c3bc58d29f09c43af150
siddharth <siddharth.pawar@vinsol.com>
2022-08-03 22:05:35 +0530

    Updated Products and Users Migrations - Updated Product migration to use change_table to add columns - Updated User migration to use add_column to add column

--- db/migrate/20220729155858_add_columns_to_products.rb
+++ db/migrate/20220729155858_add_columns_to_products.rb
@@ -1,7 +1,9 @@
 class AddColumnsToProducts < ActiveRecord::Migration[7.0]
   def change
-    add_column :products, :enabled, :boolean
-    add_column :products, :discount_price, :decimal
-    add_column :products, :permalink, :string
+    change_table :products do |t|
+      t.column :enabled, :boolean
+      t.column :discount_price, :decimal
+      t.column :permalink, :string
+    end
   end
 end
--- db/migrate/20220729161352_add_email_to_users.rb
+++ db/migrate/20220729161352_add_email_to_users.rb
@@ -1,7 +1,5 @@
 class AddEmailToUsers < ActiveRecord::Migration[7.0]
   def change
-    change_table :users do |t|
-      t.column :email, :string
-    end
+    add_column :users, :email, :string
   end
 end
