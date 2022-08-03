43493c11365e96c88f80e31b9f5a01ddae021978
siddharth <siddharth.pawar@vinsol.com>
2022-08-03 22:13:18 +0530

    Removed comments from Irrevesible migration

--- db/migrate/20220729164553_irreversible_migration.rb
+++ db/migrate/20220729164553_irreversible_migration.rb
@@ -1,16 +1,5 @@
 class IrreversibleMigration < ActiveRecord::Migration[7.0]
   def change
     change_column :users, :name, :text
-
-    ## To create above changes reversible
-    # reversible do |dir|
-    #   dir.up do
-    #     change_column :users, :name, :string
-    #   end
-
-    #   dir.down do
-    #     change_column :users, :name, :string
-    #   end
-    # end
   end
 end
