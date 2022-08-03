8ba270f24e2aacc24883dee768901e5576e8bbf8
Siddharth Pawar <42922086+Bhaikko@users.noreply.github.com>
2022-08-03 18:46:54 +0530

    4. Admin not allowed to Update and Destroy 	a. User should not be destroyed if email is 'admin@depot.com' 	b. User should not be updated if email is 'admin@depot.com'

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   # Validates that the two passwords match in field
   has_secure_password
 
+  before_destroy :ensure_not_admin
+  
   ## Creating Transaction/Trigger that will rollback when last user deleted
   after_destroy :ensure_an_admin_remains
 
@@ -15,12 +17,15 @@ class User < ApplicationRecord
     UserMailer.welcome(user).deliver_now
   end
 
-  class Error < StandardError
-  end
+  before_update :ensure_not_admin
 
   private def ensure_an_admin_remains
     if User.count.zero?
       raise Error.new "Cant't delete last user"
     end
+  end
+
+  private def ensure_not_admin
+    raise AdminImmutableException if email_was == 'admin@depot.com'
   end
 end
