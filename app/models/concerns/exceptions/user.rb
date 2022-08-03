1496298aa51e58e8debfb358fc211ece688ef93e
Siddharth Pawar <42922086+Bhaikko@users.noreply.github.com>
2022-08-03 18:46:54 +0530

    4. Admin not allowed to Update and Destroy 	a. User should not be destroyed if email is 'admin@depot.com' 	b. User should not be updated if email is 'admin@depot.com'

--- app/models/concerns/exceptions/user.rb
+++ app/models/concerns/exceptions/user.rb
@@ -1,4 +1,11 @@
 module Exceptions::User
   class Error < StandardError
   end
+<<<<<<< HEAD
 end
+=======
+
+  class AdminImmutableException < StandardError
+  end
+end
+>>>>>>> ae7cbcf (4. Admin not allowed to Update and Destroy)
