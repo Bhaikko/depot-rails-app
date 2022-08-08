fc0041d54d8c289aa909f30bbc567d0604271bf2
siddharth <siddharth.pawar@vinsol.com>
2022-08-08 17:20:39 +0530

    1. Validation added for Category and Sub-categories         a. Category's name should be mandatory 	b. All root category should be unique 	c. For each category, name of its sub_categories should be unique 	d. Uniqueness validations should only run if name is present 	e. Sub category cannot have any child category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,16 @@
 class Category < ApplicationRecord
+  belongs_to :parent, class_name: 'Category', optional: true
+
+  validates :name, presence: true
+
+  validates :name, 
+    uniqueness: {
+      scope: :parent
+    },
+    unless: -> (category) { category.name.blank? }
+    
+  validates_each :parent do |record, attr, value|
+    current_parent = record.parent
+    record.errors.add attr, "can have only one level of nesting" if current_parent && current_parent.parent
+  end
 end
