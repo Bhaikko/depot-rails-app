a1e9b58a3e269df3740b253f695a1b2247e6087f
siddharth <siddharth.pawar@vinsol.com>
2022-08-12 18:35:01 +0530

    Email presence required added as validation in User Model - Moved VALID_EMAIL_REGEX to config/initializers/constants.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i
-
-  validates :name, presence: true, uniqueness: true
+  validates :name, :email, presence: true, uniqueness: true
 
   validates :email, uniqueness: true, format: {
     with: VALID_EMAIL_REGEX
