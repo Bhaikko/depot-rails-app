0e9c811ea3295819432dcd2ca69437e81beece0b
siddharth <siddharth.pawar@vinsol.com>
2022-08-13 15:23:12 +0530

    Added Validations for Product price and discount_price - Added numericality validation for Product price and discount_price - Updated comparison validation for Product price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,31 +1,33 @@
 class Product < ApplicationRecord
   IMAGE_VALIDATION_REGEX = %r{\.(gif|jpg|png)\z}i.freeze
-  NO_SPACE_AND_SPL_CHARACTER_REGEX = /\A[a-z0-9-]+\z/i.freeze
-  MIN_3_WORDS_SEP_BY_HYPEN_REGEX = /\w+-\w+-\w+/.freeze
-  MATCH_WORD_REGEX = /[a-z0-9]+/i.freeze
+  ALPHA_NUMERIC_HYPHEN_REGEX = /\A[a-z0-9-]+\z/i.freeze
+  ALPHA_NUMERIC_REGEX = /[a-z0-9]+/i.freeze
 
   has_many :line_items
-  # specifying indirect relationship through another entity
   has_many :orders, through: :line_items
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
-  # Adding validation related to Product text field for not being empty
-  validates :title, :description, :image_url, presence: true
+  validates :title, :description, :image_url, :price, :discount_price, presence: true
 
-  # Adding validation related to price being positive number
   validates :price, 
-    numericality: { 
-      greater_than: :discount_price,
-      message: "must be greater than Discount price"
-    }, 
-    unless: -> (x) { x.price.blank? }
+    numericality: { greater_than: 0 },
+    if: -> (x) { x.price.present? }
 
-  # Validation for title being unique
+  validates :discount_price,
+    numericality: { greater_than: 0 },
+    if: -> (x) { x.discount_price.present? }
+    
+  validates :price,
+    comparison: { 
+      greater_than: :discount_price,
+      message: 'must be greater than discount price'
+    },
+    if: -> (x) { x.discount_price? }
+
   validates :title, uniqueness: true
 
-  # Validation of url using regex
-  validates :image_url, allow_blank: true, format: {
+  validates :image_url, format: {
     with:   IMAGE_VALIDATION_REGEX,
     message: 'must be a URL for GIF, JPG, or PNG image.'
   }
@@ -33,7 +35,7 @@ class Product < ApplicationRecord
   validates :permalink, uniqueness: {
     message: "Should be unique"
   }, format: {
-    with: NO_SPACE_AND_SPL_CHARACTER_REGEX,
+    with: ALPHA_NUMERIC_HYPHEN_REGEX,
     message: "cannot have special character and no space allowed"
   } 
   
@@ -42,20 +44,15 @@ class Product < ApplicationRecord
   end
 
   validates_each :description do |record, attr, value|
-    num_of_words = value.scan(MATCH_WORD_REGEX).length
+    num_of_words = value.scan(ALPHA_NUMERIC_REGEX).length
     record.errors.add :description, "should have between 5 and 10 words" if num_of_words < 5 || num_of_words > 10
   end
 
   validates :image_url, url: true
 
-  ## Creating hook method, executed before rails attempt to destory row in database
   private def ensure_not_referenced_by_any_line_item
-    # ensure that there are no line items referencing this product
     unless line_items.empty?
-      # same object used by validations to store errors
       errors.add(:base, 'Line Items present')
-
-      # If hook method throws abort then row is not destroyed
       throw :abort
     end
   end
