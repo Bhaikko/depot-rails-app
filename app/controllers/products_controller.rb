802c56a7bed6007ae04b7914a97c5f86c8fca433
siddharth <siddharth.pawar@vinsol.com>
2022-08-17 11:58:53 +0530

    1. Select box for Category selection for Product added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,27 +2,23 @@ class ProductsController < ApplicationController
   include ProductsHelper
 
   before_action :set_product, only: %i[ show edit update destroy ]
+  before_action :set_categories, only: [:new, :create, :edit, :update]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_product
   
-  # GET /products or /products.json
   def index
     @products = Product.all.order(:title)
   end
 
-  # GET /products/1 or /products/1.json
   def show
   end
 
-  # GET /products/new
   def new
     @product = Product.new
   end
 
-  # GET /products/1/edit
   def edit
   end
 
-  # POST /products or /products.json
   def create
     @product = Product.new(product_params)
 
@@ -37,19 +33,14 @@ class ProductsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /products/1 or /products/1.json
   def update
     respond_to do |format|
       if @product.update(product_params)
         format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
 
-        ## Using Channel to send entire catalog every time update is made
-        @products = Product.all.order(:title) # Required as this is used in store/index
-        ## render_to_string renders according to same rules as render,
-        # but returns the result in string instead of sending it as response body to browser
-        # data sent as key-value pair
-        # layout: false specifies that we only want view and not entire application layout page
+        @products = Product.all.order(:title)
+
         ActionCable.server.broadcast('products', { 
           html: render_to_string('store/index', layout: false) 
         })
@@ -60,7 +51,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  # DELETE /products/1 or /products/1.json
   def destroy
     @product.destroy
 
@@ -70,29 +60,29 @@ class ProductsController < ApplicationController
     end
   end
 
-  # /products/1/who_bought.atom
   def who_bought
     @product = Product.find(params[:id])
     @latest_order = @product.orders.order(:updated_at).last
     if stale?(@latest_order)
       respond_to do |format|
-        # This will look for template name who_bought.atom.builder
         format.atom
       end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    def set_categories
+      @categories = Category.all.pluck(:name, :id)
+    end
+
     def product_params
       params.require(:product).permit(
         :title, :description, :image_url, :price,
-        :enabled, :discount_price, :permalink
+        :enabled, :discount_price, :permalink, :category_id
       )
     end
 
