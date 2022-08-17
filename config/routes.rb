400053bc671df7303662c0eb7dda212e5ef0e776
siddharth <siddharth.pawar@vinsol.com>
2022-08-17 10:19:07 +0530

    Category View and Controller added - Updated routes for Category

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,10 @@ Rails.application.routes.draw do
   resources :orders
   resources :line_items
   resources :carts
+  resources :categories
   
+  root 'store#index', as: 'store_index'
+
   resources :products do
     get :who_bought, on: :member
   end
