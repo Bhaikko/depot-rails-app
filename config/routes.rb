eb70e92da9876c6fa7f84f8b6d3ca3d1aa5c3efe
siddharth <siddharth.pawar@vinsol.com>
2022-08-19 10:48:26 +0530

    /users/orders route added - Added routing config in routes.rb - Added action and view for user_orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index'
+
   get 'admin' => 'admin#index'
+  get '/users/orders', to: 'orders#user_orders'
   
-  # Mapping custom routes to actions in sessions controller
   controller :sessions do
-    # both login mapped to :new and :create method
-    # only difference is type of request being GET and POST
     get 'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
@@ -15,15 +15,9 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   
-  # Creating Store as Root URL of App 
-  # as: option creates store_index_path and store_index_url methods for tests
-  # store#index specifying class and method to use for action request
-  root 'store#index', as: 'store_index'
-
   resources :products do
     get :who_bought, on: :member
   end
 
   resources :support_requests, only: [ :index, :update ]
-
 end
