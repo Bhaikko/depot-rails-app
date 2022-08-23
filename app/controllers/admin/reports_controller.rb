c59c36035b86d55799c58ec71880b4c55e65e8c1
siddharth <siddharth.pawar@vinsol.com>
2022-08-23 16:45:44 +0530

    Date fields added on /admin/reports for order filtering - Updated Controller to handle from and to fields - Updated View to show error if fields absent

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,10 +1,17 @@
 class Admin::ReportsController < ApplicationController
   def index
-    if params[:from]
+    @from = params[:from]
+    @to = params[:to]
+
+    if params[:from].present? && params[:to].blank? || params[:from].blank? && params[:to].present?
+      flash.now[:notice] = 'Please mention both From and To field'
+      render :index and return
+    end
+
+    if params[:from].present? && params[:to].present?
       @orders = Order.by_date(params[:from], params[:to])
     else
       @orders = Order.by_date(5.days.ago, Time.now)
-    end
-
+    end 
   end
 end
