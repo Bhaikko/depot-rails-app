560e7270a0c5f2df6a0cfc4b97652d82ad46ab2a
siddharth <siddharth.pawar@vinsol.com>
2022-08-28 13:56:50 +0530

    Add firefox browser constraint for all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,40 @@
 Rails.application.routes.draw do
-  CATEGORY_ID_REGEX = /[\d]+/
+  CATEGORY_ID_REGEX = /[\d]+/.freeze
+  FIREFOX_BROWSER_REGEX = /firefox/i.freeze
 
-  root 'store#index', as: 'store_index'
+  scope constraints: -> (req) { req.headers['User-Agent'] !~ FIREFOX_BROWSER_REGEX } do
+    root 'store#index', as: 'store_index'
   
-  controller :sessions do
-    get 'login' => :new
-    post 'login' => :create
-    delete 'logout' => :destroy
-  end
-
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-
-  resources :products, path: '/books' do
-    get :who_bought, on: :member
-  end
-
-  resources :support_requests, only: [ :index, :update ]
+    get 'admin' => 'admin#index'
+    
+    controller :sessions do
+      get 'login' => :new
+      post 'login' => :create
+      delete 'logout' => :destroy
+    end
   
-  resources :categories do
-    resources :products, path: '/books', as: 'books', constraints: { category_id: CATEGORY_ID_REGEX }
-    resources :products, path: '/books', as: 'books', to: redirect('/')
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
+  
+    resources :products, path: '/books' do
+      get :who_bought, on: :member
+    end
+  
+    resources :support_requests, only: [ :index, :update ]
+    
+    resources :categories do
+      resources :products, path: '/books', as: 'books', constraints: { category_id: CATEGORY_ID_REGEX }
+      resources :products, path: '/books', as: 'books', to: redirect('/')
+    end
+  
+    namespace :admin do
+      get 'reports', to: 'reports#index'
+      get 'categories', to: 'categories#index'
+    end
+  
+    get 'my-orders', to: 'users#orders'
+    get 'my-items', to: 'users#line_items'  
   end
-
-  namespace :admin do
-    get 'reports', to: 'reports#index'
-    get 'categories', to: 'categories#index'
-    get '/', to: redirect('/admin/reports')
-  end
-
-  scope 'admin' do 
-    get 'categories', to: 'categories#admin_categories'
-  end
-
-  get 'my-orders', to: 'users#orders'
-  get 'my-items', to: 'users#line_items'  
 end
