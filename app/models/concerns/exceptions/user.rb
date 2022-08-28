17acf465db23f93511859838b652417cc16adcc0
siddharth <siddharth.pawar@vinsol.com>
2022-08-28 13:16:02 +0530

    Fix products layout

--- app/models/concerns/exceptions/user.rb
+++ app/models/concerns/exceptions/user.rb
@@ -1,11 +1,4 @@
 module Exceptions::User
   class Error < StandardError
   end
-<<<<<<< HEAD
 end
-=======
-
-  class AdminImmutableException < StandardError
-  end
-end
->>>>>>> ae7cbcf (4. Admin not allowed to Update and Destroy)
