a2f627431957200d4b798f70d73df4eb5fdfb59a
siddharth <siddharth.pawar@vinsol.com>
2022-08-29 11:34:33 +0530

    Remove validation message in LineItem model

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,12 +2,8 @@ class LineItem < ApplicationRecord
   belongs_to :order, optional: true
   belongs_to :product
   belongs_to :cart, optional: true, counter_cache: true
-
-  validates :product, uniqueness: { 
-    scope: :cart,
-    message: "has already been taken in Cart."
-  }, 
-  if: :cart
+  
+  validates :product, uniqueness: { scope: :cart }, if: :cart
 
   def total_price
     product.price * quantity
