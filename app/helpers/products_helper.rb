75f726470c9fd2883657da2a4f44dc44a4de6023
siddharth <siddharth.pawar@vinsol.com>
2022-08-29 12:44:19 +0530

    Add current user rating in Product partial

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -10,4 +10,12 @@ module ProductsHelper
   def generate_ratings
     (0.0..5.0).step(0.5).to_a
   end
+
+  def average_rating(product)
+    product.ratings.average(:rating) || 0
+  end
+
+  def current_product_rating_by_user(ratings, current_user)
+    ratings.find { |rating| rating.user_id == current_user.id }.try(:rating) || 0
+  end
 end
