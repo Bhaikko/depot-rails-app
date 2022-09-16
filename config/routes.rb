128cf27110c85c5a51c443ed57e2610528af6d19
siddharth <siddharth.pawar@vinsol.com>
2022-09-16 14:53:38 +0530

    Refactor route matching for Firefox browser

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,35 @@
 Rails.application.routes.draw do
-  scope constraints: -> (req) { req.headers['User-Agent'] !~ FIREFOX_BROWSER_REGEX } do
-    root 'store#index', as: 'store_index'
-    
-    controller :sessions do
-      get 'login' => :new
-      post 'login' => :create
-      delete 'logout' => :destroy
-    end
+  root 'store#index', as: 'store_index'
+  match '*path', to: redirect('404'), via: :all, constraints: -> (req) { req.headers['User-Agent'] =~ FIREFOX_BROWSER_REGEX }
   
-    resources :users
-    resources :orders
-    resources :line_items
-    resources :carts
-  
-    resources :products, path: '/books' do
-      get :who_bought, on: :member
-    end
-  
-    resources :support_requests, only: [ :index, :update ]
-    
-    resources :categories do
-      resources :products, path: '/books', as: 'books', constraints: { category_id: CATEGORY_ID_REGEX }
-      resources :products, path: '/books', as: 'books', to: redirect('/')
-    end
-  
-    namespace :admin do
-      get 'reports', to: 'reports#index'
-      get 'categories', to: 'categories#index'
-      get '/', to: redirect('/admin/reports')
-    end
-  
-    get 'my-orders', to: 'users#orders'
-    get 'my-items', to: 'users#line_items'  
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
   end
+
+  resources :users
+  resources :orders
+  resources :line_items
+  resources :carts
+
+  resources :products, path: '/books' do
+    get :who_bought, on: :member
+  end
+
+  resources :support_requests, only: [ :index, :update ]
+  
+  resources :categories do
+    resources :products, path: '/books', as: 'books', constraints: { category_id: CATEGORY_ID_REGEX }
+    resources :products, path: '/books', as: 'books', to: redirect('/')
+  end
+
+  namespace :admin do
+    get 'reports', to: 'reports#index'
+    get 'categories', to: 'categories#index'
+    get '/', to: redirect('/admin/reports')
+  end
+
+  get 'my-orders', to: 'users#orders'
+  get 'my-items', to: 'users#line_items'  
 end
